145e16efe93293eb45a9ecf05e1ce54f5dc2b80f
Baptiste Riffard <briffard@c1r1p5.hive.fi>
2022-05-18 17:03:08 +0300

    Add html.page

---
+++ script.js
@@ -0,0 +1,13 @@
+// const title = document.getElementById('text')
+// const message_1 = "Welcome to Roger Skyline 1"
+// function typewriter(word, index){
+//     if(index < word.length) {
+//         setTimeout(() => {
+//             text.innerHTML += `<span>${word[index]}</span>`
+//             typewriter(message_1, index +1)
+//         }, 200)
+//     }
+// }
+// setTimeout(() => {
+//     typewriter(message_1, 0)
+// }, 500);
